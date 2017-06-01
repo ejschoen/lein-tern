dea2595bdff0517a312828146bdf35dff38ecf16
ejschoen <ejschoen@i2kconnect.com>
2017-06-01 08:27:25 -0500

    Add :update command

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cc.artifice/lein-tern "0.5.0"
+(defproject cc.artifice/lein-tern "0.5.1"
   :description "Migrations as data"
   :url "http://github.com/artifice-cc/lein-tern"
   :license {:name "MIT"
